87ae1bd55b74d1ad528318c96824c0f17f50d5ff
Volha_Mashnitskaya <volha_mashnitskaya@epam.com>
2020-11-03 11:15:16 +0300

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors())
 
 
 app.use('/api/auth/', require('./routes/auth.routes'));
